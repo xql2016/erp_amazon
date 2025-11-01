d204346983303dc857082fded98a0808ea807438
qinglong.xql <qinglong.xql@cainiao.com>
2025-11-01 22:43:32 +0800

    fix bug

--- src/main/java/tools/BidUtils.java
+++ src/main/java/tools/BidUtils.java
@@ -7,10 +7,10 @@ public class BidUtils {
 
     public static Double getAdPlacementBid(AdPlacement adPlacement) {
         String nowBid = adPlacement.getBid();
-        if(StringUtils.isBlank(nowBid) || "null".equalsIgnoreCase(nowBid)) {
-            if(null != adPlacement.getReal_bid()) {
+        if(StringUtils.isBlank(nowBid) || "null".equalsIgnoreCase(nowBid) || "0.00".equalsIgnoreCase(nowBid) || 0 == Double.parseDouble(nowBid)) {
+            if(null != adPlacement.getReal_bid() && 0 != adPlacement.getReal_bid()) {
                 return adPlacement.getReal_bid();
-            } else if(null != adPlacement.getDefault_bid()) {
+            } else if(null != adPlacement.getDefault_bid() && 0 != adPlacement.getDefault_bid()) {
                 return adPlacement.getDefault_bid();
             }
             return 0.0;
